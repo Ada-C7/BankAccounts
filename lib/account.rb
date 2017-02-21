63ac23fdd313b3f3b441f1282869e9c63c36a84b
sai samant <saisamant@gmail.com>
2017-02-21 15:21:51 -0800

    passes initialize tests

--- lib/account.rb
+++ lib/account.rb
@@ -1,21 +1,24 @@
 module Bank
   class Account
-    def initialize ID, initial_balance
-      @id = ID
-      if initial_balance >0
-        @initial_balance
+    attr_reader :id, :balance
+
+    def initialize id, balance
+      @id = id
+      if balance >= 0
+        @balance = balance
       else
         raise ArgumentError.new "Initial balance must be more than zero."
-      @balance = @initial_balance
+      end
+      
+    end
+
+    def withdraw(new_withdrawal)
+
+    end
+
+    def deposit(new_deposit)
+
+    end
 
   end
-
-  def withdraw(new_withdrawal)
-    
-  end
-
-  def deposit(new_deposit)
-
-  end
-
 end

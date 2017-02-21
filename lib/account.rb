06bac15107b7e66b49a4c15e273cfa8045f85ff3
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-02-21 14:54:30 -0800

    setting up structure of document

---
+++ lib/account.rb
@@ -0,0 +1,17 @@
+module Bank
+
+  class Account
+    # creates a new account with an ID and an initial balance
+    
+    # creates a withdraw method that accepts a single parameter
+    # which represents the amount of money that will be withdrawn.
+    # and returns the updated account balance.
+
+    # creates a deposit method that accepts a single parameter
+    # which represents the amount of money that will be deposited
+    # and returns the updated account balance.
+
+    # creates a check_balance method to access
+    # the current balance of an account at any time.
+  end
+end

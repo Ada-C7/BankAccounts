fecbafa2090494d3b3a79e1f17103948abf85c24
sai samant <saisamant@gmail.com>
2017-02-21 14:57:57 -0800

    added empty deposit method

--- lib/account.rb
+++ lib/account.rb
@@ -1,11 +1,16 @@
 module Bank
   class Account
     def initialize ID, initial_balance
-
+      @id
+      @initial_balance
+      @balance
 
   end
 
   def withdraw
+  end
+
+  def deposit
   end
 
 end

e2ee42973fa265f0911421a72b65cedfee1311c9
Spatterjaaay <aja.pursova@gmail.com>
2017-02-21 15:57:23 -0800

    Added raising an Error when balance is lower than zero

--- lib/account.rb
+++ lib/account.rb
@@ -5,11 +5,11 @@ module Bank
 
     def initialize id, balance
       @id = id
-      @balance = balance
-      # if balance > 100
-      #   @balance = balance
-      # else raise ArgumentError.new "Enter a positive balance or 0."
-      # end
+
+      if balance > 0
+        @balance = balance
+      else raise ArgumentError.new
+      end
 
     end
   end

301b189b3c348d983ba9e6cbe5ae1c3ed10c7571
sai samant <saisamant@gmail.com>
2017-02-21 15:47:14 -0800

    added code and test files for Owner class

---
+++ lib/owner.rb
@@ -0,0 +1,5 @@
+module Bank
+  class Owner
+    
+  end
+end
---
+++ specs/owner_spec.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+require_relative '../lib/owner'
+
+Minitest::Reporters.use!
+
+describe "Test Owner class" do
+  
+end

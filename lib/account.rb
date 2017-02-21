2f57d521e10ba573757a56dd276cd07f044351ee
sai samant <saisamant@gmail.com>
2017-02-21 15:02:02 -0800

    added error for negative initial balance

--- lib/account.rb
+++ lib/account.rb
@@ -1,16 +1,21 @@
 module Bank
   class Account
     def initialize ID, initial_balance
-      @id
-      @initial_balance
-      @balance
+      @id = ID
+      if initial_balance >0
+        @initial_balance
+      else
+        raise ArgumentError.new "Initial balance must be more than zero."
+      @balance = @initial_balance
 
   end
 
-  def withdraw
+  def withdraw(new_withdrawal)
+    
   end
 
-  def deposit
+  def deposit(new_deposit)
+
   end
 
 end

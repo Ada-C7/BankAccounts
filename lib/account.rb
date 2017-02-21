0aca622575a08ebc371364cf3e0c1ebbe233fcb0
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-02-21 15:09:46 -0800

    passing first 3 tests

--- lib/account.rb
+++ lib/account.rb
@@ -1,5 +1,18 @@
 module Bank
   class Account
 
+    attr_reader :id, :balance
+    def initialize(id, balance)
+      if balance < 0
+        raise ArgumentError.new "Can't be negative starting balance"
+      else
+        @id = id
+      end
+      @balance = balance
+    end
+
+    
+
+
   end
 end

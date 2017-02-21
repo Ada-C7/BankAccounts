11b9cbade8b3761505dc58798e76d6a54c9bcada
Ann Dai <gofarann@gmail.com>
2017-02-21 15:56:58 -0800

    fixed output error in withdraw method

--- lib/account.rb
+++ lib/account.rb
@@ -1,17 +1,44 @@
 module Bank
 
   class Account
+    attr_accessor :balance, :id
 
-    def initialize
+
+    def initialize(account_id, initial_balance)
+      if initial_balance >= 0
+        @balance = initial_balance
+      else
+        raise ArgumentError.new "initial balance must be greater or equal to 0"
+      end
+      @id = account_id
     end
 
-    def withdraw
+    def withdraw(withdrawl_amount)
+      if withdrawl_amount > 0
+        if @balance - withdrawl_amount >= 0
+          @balance -= withdrawl_amount
+        else
+          print "your balance will be negative"
+          return @balance
+        end
+      else
+        raise ArgumentError.new "withdrawl must be greater than 0"
+        return @balance
+      end
     end
 
-    def deposit
+    def deposit(deposit_amount)
+      if deposit_amount > 0
+        @balance += deposit_amount
+        return @balance
+      else
+        raise ArgumentError.new "deposits must be greater than 0"
+        return @balance
+      end
     end
 
     def balance
+      return @balance
     end
 
   end

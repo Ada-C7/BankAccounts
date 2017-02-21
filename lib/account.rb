5d3cb81d92971e5463821929920480b5fa0aec4c
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-02-21 15:24:34 -0800

    Create Baseline Skeleton methods

---
+++ lib/account.rb
@@ -0,0 +1,23 @@
+# Baseline
+module Bank
+
+  class Accound
+
+    attr_reader :balance
+
+    def initialize (id, initial_balnce)
+      @id = id
+      @balance = initial_balnce
+
+    end
+
+
+
+    
+
+
+  end
+
+
+
+end

815583b204b924a0fca134b4c8f98f115e23383a
add2point71dots <alcland@gmail.com>
2017-02-21 15:23:18 -0800

    Added withdraw method

--- lib/account.rb
+++ lib/account.rb
@@ -4,12 +4,21 @@ module Bank
 
     def initialize(id, balance)
       @id = id
-      if balance.abs == balance
+      if balance >= 0
         @balance = balance
       else
         raise ArgumentError.new "Balance cannot be negative"
       end
     end
 
+    def withdraw(withdrawal_amount)
+      if withdrawal_amount <= @balance && withdrawal_amount >= 0
+        @balance -= withdrawal_amount
+      elsif withdrawal_amount < 0
+        raise ArgumentError.new "You cannot withdraw a negative amount."
+      else
+        raise ArgumentError.new "You cannot withdraw more than you have in your account."
+      end
+    end
   end
 end

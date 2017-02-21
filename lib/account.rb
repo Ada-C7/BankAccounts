f6c9e1df0fb137b3a6801306e7e31dd7f6af98ef
Theresa Manney <theresa.m.17@gmail.com>
2017-02-21 15:24:18 -0800

    All tests pass for Account#initialized section

--- lib/account.rb
+++ lib/account.rb
@@ -1,4 +1,22 @@
 module Bank
+
   class Account
+    attr_reader :id, :balance
+
+    def initialize(id, balance)
+      @id = id
+
+      if balance >= 0
+        @balance = balance
+      else
+        raise ArgumentError.new "The value must be between greater than or equal to 0"
+      end
+    end
+
+    # def withdraw(amount_withdrawn)
+    #
+    # end
+
   end
+
 end

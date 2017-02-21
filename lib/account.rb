14e53393d9095544ebbb2f100bebfca8f30b9927
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-02-21 15:54:41 -0800

    add raise ArgumentError for negative initial balnce

--- lib/account.rb
+++ lib/account.rb
@@ -1,19 +1,29 @@
 # Baseline
 module Bank
 
-  class Accound
+  class Account
 
     attr_reader :balance
 
     def initialize (id, initial_balnce)
       @id = id
-      @balance = initial_balnce
-
+      if initial_balnce >= 0
+        @balance = initial_balnce
+      else
+        raise ArgumentError.new "An account can't be created with negative balance"
     end
 
 
+    end
 
-    
+    def withdraw(amount)
+      @balance -= amount
+    end
+
+    def deposit(amount)
+
+      @balance += amount
+    end
 
 
   end

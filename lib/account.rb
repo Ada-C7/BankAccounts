22f4fcf9731d1557f44f2725af04eeb83dd44631
Jackie Watanabe <sushiigirl@gmail.com>
2017-02-21 15:03:05 -0800

    added deposit method

--- lib/account.rb
+++ lib/account.rb
@@ -2,16 +2,18 @@ module Bank
   class Account
 
 
-
     def initialize (id, balance)
       @id = id
       @balance = balance
 
     end
 
-
     def withdraw(money_to_withdraw)
       @balance = @balance - money_to_withdraw
+    end
+
+    def deposit(money_to_deposit)
+      @balance = @balance + money_to_deposit
     end
 
   end

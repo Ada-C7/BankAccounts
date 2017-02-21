7d05797d0f3c800fc7f30ad4b8e557346f3b40cf
sai samant <saisamant@gmail.com>
2017-02-21 15:49:47 -0800

    initialized class

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,10 @@
 module Bank
   class Owner
-    
+    def initialize owner_hash
+      @first_name = owner_hash[:first_name]
+      @last_name = owner_hase[:last_name]
+      @address = owner_hash[:address]
+      @phone_number = owner_hash[:phone]
+    end
   end
 end

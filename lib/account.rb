8f64dba5d8322a38ebb1de2c76230e4325ba873d
AlisonZ <alisonkzerbe@gmail.com>
2017-02-21 18:14:27 -0800

    working withdraw method, needs to be tested with spec file

--- lib/account.rb
+++ lib/account.rb
@@ -1,12 +1,35 @@
 
 module Bank
     class Account
-        attr_accessor :id, :balance
+        attr_accessor :id, :balance, :withdraw
 
         def initialize (id, balance)
+            #need to call a method here that checks if balance is > 0; argument error
             @id = id
             @balance = balance
+            @withdraw
         end
+
+        def withdraw (withdraw_amount)
+            @withdraw = withdraw_amount
+            if @balance - @withdraw > 0
+                puts "Here's your cash"
+                @balance = @balance - @withdraw
+                puts "Your new balance is: $#{@balance}"
+            else
+                puts "you do not have enough money in your account for this"
+            end
+        end
+
+        def deposit
+            #make sure positive
+            #adjust balance
+        end
+
+        def new_account_check
+            #make sure that new accounts are not started with negative balance
+        end
+
 
         def show_balance
             puts "Your balance is: $#{@balance}"
@@ -17,4 +40,5 @@ module Bank
 end
 
 my_account = Bank::Account.new(16, 1000)
-my_account.show_balance
+# my_account.show_balance
+my_account.withdraw(200)

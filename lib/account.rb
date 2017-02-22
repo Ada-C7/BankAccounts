bc4db04b9df2fa4bd9bedb5e3433bcd84de2f0a5
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-02-21 16:49:56 -0800

    Add positive_amount method to check + money

--- lib/account.rb
+++ lib/account.rb
@@ -3,26 +3,44 @@ module Bank
 
   class Account
 
-    attr_reader :balance
+    attr_reader :balance, :id
 
     def initialize (id, initial_balnce)
-      @id = id
+
       if initial_balnce >= 0
         @balance = initial_balnce
       else
         raise ArgumentError.new "An account can't be created with negative balance"
+      end
+      @id = id
     end
 
+    def positive_amount(amount)
+      if amount > 0
+        true
+      else
+        raise ArgumentError.new "Invalid negative amount"
+      end
 
     end
 
     def withdraw(amount)
-      @balance -= amount
+      while positive_amount(amount)
+        if amount <= @balance
+          @balance -= amount
+        else
+          raise ArgumentError.new "You don't have sufficient funds. Max withdrawel amount is #{@balance}."
+          @balance -= amount
+        end
+        return @balance
+      end
     end
 
     def deposit(amount)
+        if positive_amount(amount)
+          @balance += amount
+      end
 
-      @balance += amount
     end
 
 

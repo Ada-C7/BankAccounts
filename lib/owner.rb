d311e5f80a54520b1194c5f0cded89e4b700615c
sai samant <saisamant@gmail.com>
2017-02-21 16:17:48 -0800

    updated hash to have keys distinct from class vars

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,10 +1,12 @@
 module Bank
   class Owner
+    attr_reader :first_name, :last_name, :address, :phone_number
+
     def initialize owner_hash
-      @first_name = owner_hash[:first_name]
-      @last_name = owner_hase[:last_name]
-      @address = owner_hash[:address]
-      @phone_number = owner_hash[:phone]
+      @first_name = owner_hash[:in_first_name]
+      @last_name = owner_hash[:in_last_name]
+      @address = owner_hash[:in_address]
+      @phone_number = owner_hash[:in_phone]
     end
   end
 end

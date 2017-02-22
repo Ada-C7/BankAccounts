d1f830285f0f637d538e001c4d8013daa6477092
Ann Dai <gofarann@gmail.com>
2017-02-22 09:25:57 -0800

    removed owner class

--- lib/account.rb
+++ lib/account.rb
@@ -40,20 +40,6 @@ module Bank
       return @balance
     end
 
-
-  end
-
-  class Owner
-    attr_accessor :name, :address, :email
-
-    def initialize(user_info_hash)
-
-      @name = user_info_hash[:name]
-      @address = user_info_hash[:address]
-      @email = user_info_hash[:email]
-
-    end
-
   end
 
 end

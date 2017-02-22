ea721dc61a159c4496e2840569bf302a909b2d95
Bo Trethewey <botrethewey@gmail.com>
2017-02-21 22:26:36 -0800

    Reduced the number of attributes in Owner class

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,21 +1,13 @@
 module Bank
   class Owner
-    attr_reader :customer_id, :first_name, :last_name, :street, :city, :zipcode, :address, :phone
+    attr_reader :customer_id, :name, :address, :phone
     def initialize owner_hash
       # assumes that owner_hash structure is
-      # {customer_id:
-      name: "ginny smith", address => {street: "122 main st.", city: "seattle", zipcode: "98144"}, phone: "2065573099" }
-      # if owner_hash.class != Hash
-      #   raise ArgumentError.new "Erroneous argument used for the constructor"
-      # else
-      @name = owner_hash[:name]
-      # @street = owner_hash[:address][:street]
-      # @city = owner_hash[:address][:city]
-      # @zipcode = owner_hash[:address][:zipcode]
-      # @address = { street: @street, city: @city, zipcode: @zipcode }
-      @address = owner_hash[:address]
+      # { customer_id: 1024, first_name: "ginny", last_name: "smith", address => {street: "123 main street", city: "seattle", state: "WA", zipcode: "12345"}, phone: "2065573099" }
+      @customer_id = owner_hash[:customer_id]
+      @name = "#{owner_hash[:first_name]} #{owner_hash[:last_name]}"
+      @address = "#{owner_hash[:address][:street]}, #{owner_hash[:address][:city]}, #{owner_hash[:address][:state]} #{owner_hash[:address][:zipcode]}"
       @phone = owner_hash[:phone]
-      #end
     end
   end
 end

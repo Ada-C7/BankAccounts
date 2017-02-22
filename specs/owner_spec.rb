d365262adb56819e112fc88a23d5592570203958
add2point71dots <alcland@gmail.com>
2017-02-21 16:38:11 -0800

    Added test for verifying arguments in initialization of Owner

--- specs/owner_spec.rb
+++ specs/owner_spec.rb
@@ -22,6 +22,24 @@ describe "Owner#initialize" do
 
     owner.must_respond_to :email
     owner.email.must_equal owner_hash[:email]
+  end
+
+  it "Raises ArgumentError when created without a name, address, or email" do
+    owner_hash1 = { address: "123 Fake Street", email: "ada@example.com" }
+    owner_hash2 = { name: "Ada", email: "ada@example.com" }
+    owner_hash3 = { name: "Ada", address: "123 Fake Street" }
+    owner_hash4 = { name: "Ada" }
+    owner_hash5 = { address: "123 Fake Street" }
+    owner_hash6 = { email: "ada@example.com" }
+    owner_hash7 = {}
+
+    proc { Bank::Owner.new(owner_hash1) }.must_raise ArgumentError
+    proc { Bank::Owner.new(owner_hash2) }.must_raise ArgumentError
+    proc { Bank::Owner.new(owner_hash3) }.must_raise ArgumentError
+    proc { Bank::Owner.new(owner_hash4) }.must_raise ArgumentError
+    proc { Bank::Owner.new(owner_hash5) }.must_raise ArgumentError
+    proc { Bank::Owner.new(owner_hash6) }.must_raise ArgumentError
+    proc { Bank::Owner.new(owner_hash7) }.must_raise ArgumentError
 
   end
 end

ea94ecf079f73207f87607b907d1ab376845e467
add2point71dots <alcland@gmail.com>
2017-02-21 16:29:06 -0800

    Added validate_owner_info method to Owner class

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,9 +3,20 @@ module Bank
     attr_reader :name, :address, :email
 
     def initialize(owner_hash)
-      @name = owner_hash[:name]
-      @address = owner_hash[:address]
-      @email = owner_hash[:email]
+      @owner_hash = owner_hash
+      @name = validate_owner_info(:name)
+      @address = validate_owner_info(:address)
+      @email = validate_owner_info(:email)
     end
+
+    # makes sure required fields are not nil or empty strings
+    def validate_owner_info(required_field)
+      if !@owner_hash.has_key?(required_field) || @owner_hash[required_field] == ""
+        raise ArgumentError.new "#{required_field} is a required field."
+      else
+        return @owner_hash[required_field]
+      end
+    end
+
   end
 end

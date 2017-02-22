d8b8b00bd42bfd737f4ef4b917bd6a240b70109b
Lynn Trickey <lynn.trickey@gmail.com>
2017-02-22 14:16:27 -0800

    Adding stubs of .all class method for Account

--- lib/account.rb
+++ lib/account.rb
@@ -4,42 +4,51 @@ module Bank
     attr_accessor :balance, :owner
     attr_reader :id
 
+    def self.all
+      
+    end
+
     def initialize(id, balance)
+
       @id = id
 
-      if balance >= 0
-        @balance = balance
-      else
+      if balance < 0
         raise ArgumentError.new "You cannot initialize a new account with a negative balance."
+      else
+        @balance = balance
       end
 
       def add_owner(owner)
+
         if owner.class == Owner
           @owner = owner
         else
           raise ArgumentError.new "You must add a class type of Owner."
         end
+
       end
 
       def withdraw(withdrawal_amount)
-        if withdrawal_amount > 0
-          if withdrawal_amount <= @balance
-            @balance -= withdrawal_amount
-          else
-            puts "You are going negative."
-            @balance
-          end
-        else
-          raise ArgumentError.new "Your withdrawal amount must be positive."
-        end
+
+      raise ArgumentError.new("Withdrawal must be >=0") if withdrawal_amount < 0
+
+      if @balance - withdrawal_amount < 0
+        puts "You are going negative."
+        return @balance
+      else
+        @balance -= withdrawal_amount
+      end
+
       end
 
       def deposit(deposit_amount)
-        if deposit_amount > 0
-          @balance += deposit_amount
-        else
-          raise ArgumentError.new "Your deposit must be greater than zero."
-        end
+
+      if deposit_amount > 0
+        @balance += deposit_amount
+      else
+        raise ArgumentError.new "Your deposit must be greater than zero."
+      end
+
       end
 
     end

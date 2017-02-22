640c6f5722c3302284f020ec72b846c1f4c6b335
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-02-22 14:09:10 -0800

    added wave 2 file

---
+++ lib/account_wave_2.rb
@@ -0,0 +1,42 @@
+module Bank
+  class Account
+
+    attr_reader :id, :balance, :owner
+    def initialize(id, balance, owner)
+      if balance < 0
+        raise ArgumentError.new "Can't be negative starting balance"
+      end
+      @id = id
+      @balance = balance
+      @owner = owner
+    end
+
+    def withdraw(withdrawal_amount)
+      if withdrawal_amount > @balance
+        puts "You cannot withdraw more than you have in your account!"
+        @balance = @balance
+      elsif withdrawal_amount < 0
+        raise ArgumentError.new "You cannot withdraw a negative amount."
+      else
+        @balance -= withdrawal_amount
+      end
+    end
+
+    def deposit(deposit_amount)
+      if deposit_amount < 0
+        raise ArgumentError.new "Cannot deposit a negative amount."
+      end
+      @balance += deposit_amount
+    end
+
+
+  end
+
+  class Owner
+    attr_reader :name, :address
+    def initialize(name, address)
+      @name = name
+      @address = address
+    end
+  end
+end

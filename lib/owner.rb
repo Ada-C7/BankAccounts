09f81d1358ca6be3a2512eb22adf068bf6fe3f0a
natalia-ku <bilberryfm@mail.ru>
2017-02-21 20:23:40 -0800

    Created a new file to store Owner class

---
+++ lib/owner.rb
@@ -0,0 +1,14 @@
+
+module Bank
+  class Owner
+    attr_reader :name, :address, :phone
+    def initialize (name, address, phone)
+      @name = name
+      @address = address
+      @phone = phone
+    end
+  end
+end
+
+owner = Bank::Owner.new("Natalia", "123 Main st ", "432332322")
+puts owner.address

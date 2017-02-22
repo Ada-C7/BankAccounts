93ad9c64b1946d56c0c5daa257c0be577ab56c5e
Kelly Souza <kellysouza99@gmail.com>
2017-02-21 16:37:33 -0800

    Wave 1 working through

--- lib/account.rb
+++ lib/account.rb
@@ -20,18 +20,28 @@ module Bank
 
     def withdraw(withdrawal_amount)
       start_balance = @balance
+
       if withdrawal_amount < 0
         raise ArgumentError.new "You cannot withdraw a negative amount"
       end
+
       if start_balance >= withdrawal_amount
         @balance = start_balance - withdrawal_amount
       else
        puts "Cannot withdraw more than is in the account"
       end
-      print @balance
       return @balance
     end
 
+    def deposit(deposit_amount)
+      start_balance = @balance
+      if deposit_amount >= 0
+        @balance = start_balance + deposit_amount
+        return @balance
+      else
+      raise ArgumentError.new "You must deposit an amount"
+      end
+    end
 
 
   end #end class Account

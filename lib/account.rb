7e3613157821c4636fdde2610fef2beba610380b
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-02-22 08:05:32 -0800

    Deposit tests pass. Broke withdraw 1/6 pass

--- lib/account.rb
+++ lib/account.rb
@@ -3,6 +3,7 @@ module Bank
     attr_reader :id, :balance
     def initialize(id, balance)
       #error message not appearing in def withdraw
+      #WOW, that balance can't be @balance. WHY?
       raise ArgumentError.new("balance must be >= 0") if balance < 0
 
       @id = id
@@ -13,13 +14,20 @@ module Bank
       # TODO: implement withdraw
       start_balance = @balance
       withdrawal_amount = amount
-      @balance = start_balance - withdrawal_amount
+      updated_balance = start_balance - withdrawal_amount
+      # if updated_balance < 0 then
+      #   raise ArgumentError.new("You cannot withdraw #{withdrawal_amount}. Your balance is #{start_balance}.")
+      #   @balance = start_balance
+      # end
     end
 
     def deposit(amount)
       # TODO: implement deposit
       start_balance = @balance
       deposit_amount = amount
+      if deposit_amount < 0
+        raise ArgumentError.new 'You cannot deposit a negative number'
+      end
       @balance = start_balance + deposit_amount
     end
   end

c718b82a8b941c278c0c17c27a0adad64dba964f
sai samant <saisamant@gmail.com>
2017-02-21 17:04:04 -0800

    added get_owner_info method

--- lib/account.rb
+++ lib/account.rb
@@ -1,6 +1,8 @@
+require_relative 'owner'
+
 module Bank
   class Account
-    attr_reader :id, :balance
+    attr_reader :id, :balance, :owner
 
     def initialize id, balance
       @id = id
@@ -9,6 +11,7 @@ module Bank
       else
         raise ArgumentError.new "Initial balance must be more than zero."
       end
+      @owner
 
     end
 
@@ -26,12 +29,29 @@ module Bank
     def deposit(new_deposit)
       if new_deposit <= 0
         raise ArgumentError.new "Your deposit amount must have a positive value"
-        
+
       else
         @balance += new_deposit
       end
 
     end
 
+    def get_owner_info(owner_hash)
+      @owner = Bank::Owner.new(owner_hash)
+    
+    end
+
+
+
   end
 end
+
+owner_hash = {
+  :in_first_name => "Grace",
+  :in_last_name => "Hopper",
+  :in_address => "456 Anytown, USA",
+  :in_phone => "206-440-0725"
+}
+
+new_account = Bank::Account.new(1375, 200)
+new_account.get_owner_info(owner_hash)

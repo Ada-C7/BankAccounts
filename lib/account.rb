b588f6a5661efb422fa931c898d7c9258af8947a
Anna Elisabeth <anna.e.boyd@gmail.com>
2017-02-22 15:53:41 -0800

    reading the csv file

--- lib/account.rb
+++ lib/account.rb
@@ -1,11 +1,14 @@
+require 'csv'
+
 module Bank
   class Account
-
-    attr_reader :id, :balance
-
-    def initialize(id, balance)
+    attr_reader :id, :balance, :date_created
+    @@Accounts = []
+    def initialize(id, balance, date_created)
       @id = id
       @balance = balance
+      @date_created = date_created
+      @@Accounts << [id, balance, date_created]
       raise ArgumentError.new("balance must be >= 0") if balance < 0
     end
 
@@ -23,14 +26,36 @@ module Bank
       raise ArgumentError.new("amount must be >= 0") if amount < 0
       return @balance += amount
     end
+
+    def self.all
+      return @@Accounts
+    end
+
+    def self.find(id)
+      @@Accounts.each do |account|
+        if account[0] == id
+          return account
+        else
+          raise ArgumentError.new("that account cannot be found")
+        end
+      end
+    end
+
+    def self.create_many_accounts
+      many_accounts = []
+      CSV.open("./support/accounts.csv").each do |line|
+          many_accounts << self.new(line[0].to_i, line[1].to_i, line[2])
+      end
+    end
+
   end
 
-  class Owner
-    attr_reader :name, :address
-    def initialize(name, address)
-      @name = name
-      @address = address
-    end
-  end
+  # class Owner
+  #   attr_reader :name, :address
+  #   def initialize(name, address)
+  #     @name = name
+  #     @address = address
+  #   end
+  # end
 
 end

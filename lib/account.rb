d93f491c9a36cfcfa47e69311797509d142b1d4f
jjousun <jjousun@gmail.com>
2017-02-21 16:25:39 -0800

    Passed Wave 1 tests

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,39 @@
+# bank account that allows new accounts, deposit and withdrawals
+# jou-jou sun
+# last edit 2/21/17
 module Bank
   class Account
-    def initialize id, balance
-      @id = 1337
-      @balance = 100.0
+    attr_reader :id, :balance
+
+    def initialize(id, balance)
+      @id = id
+      @balance = balance
+        if @balance < 0
+          raise ArgumentError.new "The starting balance must be positive"
+        end
     end
 
-  end
-end
+    def withdraw(withdrawal_amount)
+      if withdrawal_amount < 0
+        raise ArgumentError.new "Withdrawal must be positive"
+      end
+      if withdrawal_amount > @balance
+        print "Account will be in negative with this withdrawal"
+        return @balance
+      else
+        @balance -= withdrawal_amount
+        return @balance
+      end
+    end
+
+    def deposit(deposit)
+      if deposit < 0
+        raise ArgumentError.new "Deposit must be positive"
+      end
+      @balance += deposit
+      return @balance
+    end
+
+  end #end of class
+
+end #end of module

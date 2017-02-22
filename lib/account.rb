a282d51b01d912de5d8f8d9ddab51697c0282057
Kelly Souza <kellysouza99@gmail.com>
2017-02-21 16:21:52 -0800

    withdrawals working

--- lib/account.rb
+++ lib/account.rb
@@ -20,8 +20,19 @@ module Bank
 
     def withdraw(withdrawal_amount)
       start_balance = @balance
-      @balance = start_balance - withdrawal_amount
+      if withdrawal_amount < 0
+        raise ArgumentError.new "You cannot withdraw a negative amount"
+      end
+      if start_balance >= withdrawal_amount
+        @balance = start_balance - withdrawal_amount
+      else
+       puts "Cannot withdraw more than is in the account"
+      end
+      print @balance
+      return @balance
     end
+
+
 
   end #end class Account
 end #end module Bank

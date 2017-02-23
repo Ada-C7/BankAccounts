349d18b6df4b1f5912728fdd6344b4ba6574e520
Anacelia Gillette <acgillette426@gmail.com>
2017-02-22 20:58:32 -0800

    Whoops forget to uncomment a block of code

--- lib/owner.rb
+++ lib/owner.rb
@@ -14,17 +14,17 @@ module Bank
       return owner_array
     end
 
-    # def self.find(id)
-    #   CSV.open("support/accounts.csv", 'r').each do |account|
-    #     if account[0] == id
-    #       balance = Integer(account[1])
-    #       open_date = account[2]
-    #       existing_account = Account.new(id, balance, open_date)
-    #       return existing_account
-    #     end
-    #   end
-    #   raise ArgumentError.new "The ID is not associated with an account"
-    # end
+    def self.find(id)
+      CSV.open("support/accounts.csv", 'r').each do |account|
+        if account[0] == id
+          balance = Integer(account[1])
+          open_date = account[2]
+          existing_account = Account.new(id, balance, open_date)
+          return existing_account
+        end
+      end
+      raise ArgumentError.new "The ID is not associated with an account"
+    end
     attr_reader :name, :address, :phone, :id
 
 
@@ -32,7 +32,6 @@ module Bank
       @id = id
       @name = name
       @address = address
-      @phone = phone
     end
   end
 

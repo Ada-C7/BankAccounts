d7ba438fdf90143d93b96625605df93ce133c145
Haby Randall <sow.haby@gmail.com>
2017-02-22 16:10:12 -0800

    Added self.all class, starting to write tests

--- lib/account.rb
+++ lib/account.rb
@@ -1,13 +1,32 @@
+require 'csv'
 module Bank
+
+
   class Account
+
+    def self.all
+      account = []
+      CSV.open("support/accounts.csv", "r").each do |account|
+        id = account[0]
+        balance = Integer(account[1])
+        open_date = account[2]
+        new_account = Account.new(id, balance, open_date)
+        account << new_account
+      end
+      return account
+
+    end
+
     attr_reader :id, :balance
-    def initialize(id, balance)
+    def initialize(id, balance, open_date = nil)
       @id = id
       @balance = balance
       if @balance < 0
         raise ArgumentError.new "The starting balance cannot be negative"
       end
     end
+
+
 
     def withdraw(withdrawal_amount)
       if withdrawal_amount > @balance
@@ -21,11 +40,23 @@ module Bank
     end
 
     def deposit(deposit_amount)
+      #raise ArgumentError.new("amount must be >= 0") if amount < 0
       if deposit_amount < 0
         raise ArgumentError.new "The deposit must be greater than 0"
+
       else
         @balance += deposit_amount
       end
     end
   end
 end
+# def withdraw(amount)
+#   raise ArgumentError.new("amount must be >= 0") if amount < 0
+#
+#   if @balance - amount < 0
+#     puts "Oh no! Account will be negative"
+#     return @balance
+#   else
+#     @balance -= amount
+#   end
+# end

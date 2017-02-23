635442d2942784b236e70aeb6a2cb4678b9e5380
natalia-ku <bilberryfm@mail.ru>
2017-02-22 16:14:44 -0800

    added test for self.all method

--- specs/owner_spec.rb
+++ specs/owner_spec.rb
@@ -6,35 +6,40 @@ require_relative '../lib/owner'
 # Owner class inside Bank Module:
 describe "Owner#initialize" do
   it "Can be instantiated" do
-    owner = Bank::Owner.new("David", "123 Street", "32329449")
+    owner = Bank::Owner.new("David")
     owner.must_be_instance_of Bank::Owner , "Made a owner, but it is not a Owner class"
   end
-  it "Takes name, address and phone" do
+  it "Takes name" do
     name = "Natalia"
-    address = "234 Main NE St"
-    phone = "42354565"
-    owner = Bank::Owner.new(name, address, phone)
+    owner = Bank::Owner.new(name)
 
     owner.must_respond_to :name
     owner.name.must_equal name
 
-    owner.must_respond_to :address
-    owner.address.must_equal address
-
-    owner.must_respond_to :phone
-    owner.phone.must_equal phone
   end
   it "Input must be string:" do
-    owner = Bank::Owner.new("Natalia", "234 Street", "43434343434")
+    owner = Bank::Owner.new("Natalia")
     owner.name.must_be_kind_of String
-    owner.address.must_be_kind_of String
-    owner.phone.must_be_kind_of String
   end
 end
 
 describe "Owner#all" do
+  it "Owner.all returns an array" do
+    all_owners = Bank::Owner.all
+    all_owners.must_be_kind_of Array
+  end
+  it "Everything in the array is an Owner" do
+    all_owners = Bank::Owner.all
+    all_owners.each do |owner|
+      owner.must_be_kind_of Bank::Owner
+    end
+  end
+  it " The number of owners is correct" do
+    all_owners = Bank::Owner.all
+    all_owners.length.must_be :==, 12
+  end
 
-  
+
 end
 
 describe "Owner#find(id)" do

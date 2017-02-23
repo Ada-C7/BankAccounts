4d1559693da6a6e709d01bfa52469205f0a1dc0b
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-02-23 13:06:04 -0800

    added empty savings and checking class stubs

---
+++ lib/CheckingAccount.rb
@@ -0,0 +1,9 @@
+require_relative 'account'
+
+class CheckingAccount < Account
+
+  def initialize
+  end
+
+
+end
---
+++ lib/SavingsAccount.rb
@@ -0,0 +1,9 @@
+require_relative 'account'
+
+class SavingsAccount < Account
+
+  def initialize
+  end
+
+
+end

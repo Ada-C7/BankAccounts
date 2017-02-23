7e6266cc551df82391ba7b81707b127c6a1a60f5
natalia-ku <bilberryfm@mail.ru>
2017-02-22 16:15:11 -0800

    added self.all method

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,14 +1,36 @@
-
+require_relative 'account'
+require 'csv'
+require 'date'
 module Bank
   class Owner
     attr_reader :name, :address, :phone
-    def initialize (name, address, phone)
+    def initialize (name, phone="1234456", last_name="Fakelastname" , first_name="Fakefirstname", address="123 Main st", city="Bellevue", state="WA")
       @name = name
-      @address = address
-      @phone = phone
+      #@address = address
+      #@phone = phone
     end
+
+    def self.all
+      csv = CSV.read("../support/owners.csv", 'r') # object of class CSV
+      all_owners = []
+      n = 0 # current line of csv file
+      csv.length.times do |n|
+        id = csv[n][0].to_i
+        last_name = csv[n][1]
+        first_name = csv[n][2]
+        address = csv[n][3]
+        city = csv[n][4]
+        state = csv[n][5]
+        all_owners << Owner.new(id, last_name, first_name, address, city, state)
+        n += 1
+      end
+      return all_owners
+    end
+
+
   end
 end
 
-# owner = Bank::Owner.new("Natalia", "123 Main st ", "432332322")
+ owner = Bank::Owner.new("Natalia")
+ puts Bank::Owner.all
 # puts owner.address

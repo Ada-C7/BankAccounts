2fdf5d81df61c3c562dbf9688181fca0b8adcdf9
add2point71dots <alcland@gmail.com>
2017-02-22 16:27:43 -0800

    Added tests to verify new fields

--- specs/owner_spec.rb
+++ specs/owner_spec.rb
@@ -6,31 +6,43 @@ require_relative '../lib/owner'
 Minitest::Reporters.use!
 
 describe "Owner#initialize" do
-  it "Takes a name, address, and email" do
+  it "Takes all required info from a hash" do
     owner_hash = {
-      name: "Ada",
+      id: 4,
+      last_name: "Lovelace",
+      first_name: "Ada",
       address: "123 Fake Street",
-      email: "ada@example.com"
+      city: "Fake City",
+      state: "Fake State"
     }
     owner = Bank::Owner.new(owner_hash)
 
-    owner.must_respond_to :name
-    owner.name.must_equal owner_hash[:name]
+    owner.must_respond_to :id
+    owner.id.must_equal owner_hash[:id]
+
+    owner.must_respond_to :last_name
+    owner.last_name.must_equal owner_hash[:last_name]
+
+    owner.must_respond_to :first_name
+    owner.first_name.must_equal owner_hash[:first_name]
 
     owner.must_respond_to :address
     owner.address.must_equal owner_hash[:address]
 
-    owner.must_respond_to :email
-    owner.email.must_equal owner_hash[:email]
+    owner.must_respond_to :city
+    owner.city.must_equal owner_hash[:city]
+
+    owner.must_respond_to :state
+    owner.state.must_equal owner_hash[:state]
   end
 
-  it "Raises ArgumentError when created without a name, address, or email" do
-    owner_hash1 = { address: "123 Fake Street", email: "ada@example.com" }
-    owner_hash2 = { name: "Ada", email: "ada@example.com" }
-    owner_hash3 = { name: "Ada", address: "123 Fake Street" }
-    owner_hash4 = { name: "Ada" }
-    owner_hash5 = { address: "123 Fake Street" }
-    owner_hash6 = { email: "ada@example.com" }
+  it "Raises ArgumentError when created without required fields" do
+    owner_hash1 = { last_name: "Lovelace", first_name: "Ada", address: "123 Fake Street", city: "Fake City", state: "Fake State" }
+    owner_hash2 = { id: 4, first_name: "Ada", address: "123 Fake Street", city: "Fake City", state: "Fake State" }
+    owner_hash3 = { id: 4, last_name: "Lovelace", address: "123 Fake Street", city: "Fake City", state: "Fake State" }
+    owner_hash4 = { id: 4, last_name: "Lovelace", first_name: "Ada", city: "Fake City", state: "Fake State" }
+    owner_hash5 = { id: 4, last_name: "Lovelace", first_name: "Ada", address: "123 Fake Street", state: "Fake State" }
+    owner_hash6 = { id: 4, last_name: "Lovelace", first_name: "Ada", address: "123 Fake Street", city: "Fake City" }
     owner_hash7 = {}
 
     proc { Bank::Owner.new(owner_hash1) }.must_raise ArgumentError

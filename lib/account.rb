3500337592b00a6b1850c6f66123fd6c5a8aeda1
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-02-23 14:02:56 -0800

    reverted to Wave 1 code

--- lib/account.rb
+++ lib/account.rb
@@ -1,75 +1,14 @@
 module Bank
   class Account
-    attr_reader :id, :balance, :open_date, :all_accounts
-    # def initialize(id, balance)
-    #   #error message not appearing in def withdraw
-    #   #WOW, that balance can't be @balance. WHY?
-    #   raise ArgumentError.new("balance must be >= 0") if balance < 0
-
-    # end
-
+    attr_reader :id, :balance
     def initialize(id, balance)
-      @id = id  #still need this
-      @balance = balance #still need this
+      #error message not appearing in def withdraw
+      #WOW, that balance can't be @balance. WHY?
+      raise ArgumentError.new("balance must be >= 0") if balance < 0
 
-      @all_accounts = []
-      CSV.read("accounts.csv").each do |line|
-        @all_accounts[line.first] = line[1..-1]
-      end
-
-      #make these seperate methods instead? - OH! That's why
-      #we have self.find(id)!
-      # @id = []
-      # @balance = []
-      # @open_date = []
-      # CSV.read("accounts.csv").each do |account|
-      #   account.each do |index|
-          # @id << account[index]
-          # @balance << account[index]
-          # @open_date << account[index]
-          # index += 1
-      #   end
-      # end
+      @id = id
+      @balance = balance
     end
-
-    def self.all
-      return @all_accounts
-    end
-
-    def self.id
-      @all_accounts.each do |accounts|
-        accounts.each_index do |i|
-          return i if i == 0
-        end
-      end
-    end
-
-    def self.balance
-      @all_accounts.each do |accounts|
-        accounts.each_index do |i|
-          return i if i == 1
-        end
-      end
-    end
-
-    def self.open_date
-      @all_accounts.each do |accounts|
-        accounts.each_index do |i|
-          return i if i == 2
-        end
-      end
-    end
-
-    def self.find(id)
-      #https://ruby-doc.org/core-2.2.3/Enumerable.html#method-i-find
-      @all_accounts.each do |account|
-        if account.id == id
-          return account
-        end
-      end
-    end
-
-##############WAVE 1##################
 
     def withdraw(amount)
       start_balance = @balance

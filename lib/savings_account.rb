2d47d9b3218e1148de6c9b33d2f4c035b9636bde
Spatterjaaay <aja.pursova@gmail.com>
2017-02-23 15:33:25 -0800

    Added initialize method

--- lib/savings_account.rb
+++ lib/savings_account.rb
@@ -1,4 +1,12 @@
 module Bank
   class SavingsAccount < Account
+    def initialize(id, start_balance)
+      super(id, start_balance)
+
+      if start_balance >= 10.0
+        @balance = start_balance
+      else raise ArgumentError.new
+      end
+    end
   end
 end

79ba7eb35ef47a726b8cc01b8e8e9fe22b95f2a5
Spatterjaaay <aja.pursova@gmail.com>
2017-02-22 16:17:07 -0800

    Created class method All, read CSV file in it and created an array of individual accounts

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,29 @@
+require 'csv'
+
 module Bank
   class Account
     attr_reader :id
     attr_accessor :balance
 
+
+    # should get info from csv
+    # create new instances of Account based on the csv info
+    # create an array of Account instances
+    # and return it
+    def self.all
+      accounts_array = CSV.read("/Users/alena/Documents/Ada/projects/BankAccounts/support/accounts.csv")
+
+      new_accounts = []
+
+      accounts_array.each do |line|
+        new_accounts << Account.new(line[0].to_i, line[1].to_i)
+      end
+      return new_accounts
+    end
+
     def initialize id, start_balance
+      # raise ArgumentError.new("amount must be >= 0") is amount < 0
+
       @id = id
 
       if start_balance >= 0
@@ -13,21 +33,30 @@ module Bank
     end
 
     def withdraw(withdrawal_amount)
+      # dont need an else beacuse it stops the program
 
+      # raise ArgumentError.new("amount must be >= 0") is amount < 0
       if withdrawal_amount < 0
         raise ArgumentError.new
       elsif withdrawal_amount > @balance
         print "You are withdrawing too much!"
+        return @balance
       else
         @balance -= withdrawal_amount
       end
 
-      @balance
     end
 
     def deposit(deposit_amount)
+      # raise ArgumentError.new("amount must be >= 0") is amount < 0
+      if deposit_amount < 0
+        raise ArgumentError.new
+      end
+
       @balance += deposit_amount
     end
 
   end
 end
+
+puts Bank::Account.all

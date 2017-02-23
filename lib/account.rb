dc1b8f225a8166f075abd7594164d6a630857e88
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-02-23 07:11:09 -0800

    initialize, self.all, self.find(id)

--- lib/account.rb
+++ lib/account.rb
@@ -1,13 +1,47 @@
 module Bank
   class Account
-    attr_reader :id, :balance
-    def initialize(id, balance)
-      #error message not appearing in def withdraw
-      #WOW, that balance can't be @balance. WHY?
-      raise ArgumentError.new("balance must be >= 0") if balance < 0
+    attr_reader :id, :balance, :open_date, @accounts
+    # def initialize(id, balance)
+    #   #error message not appearing in def withdraw
+    #   #WOW, that balance can't be @balance. WHY?
+    #   raise ArgumentError.new("balance must be >= 0") if balance < 0
+    #
+    #   @id = id
+    #   @balance = balance
+    # end
 
-      @id = id
-      @balance = balance
+    def initialize
+      @accounts = []
+      CSV.read("accounts.csv").each do |line|
+        @accounts[line.first] = line[1..-1]
+      end
+
+      #make these seperate methods instead? - OH! That's why
+      #we have self.find(id)!
+      # @id = []
+      # @balance = []
+      # @open_date = []
+      # CSV.read("accounts.csv").each do |account|
+      #   account.each do |index|
+          # @id << account[index]
+          # @balance << account[index]
+          # @open_date << account[index]
+          # index += 1
+      #   end
+      # end
+    end
+
+    def self.all
+      return @accounts
+    end
+
+    def self.find(id)
+      #https://ruby-doc.org/core-2.2.3/Enumerable.html#method-i-find
+      @accounts.each do |outside_array|
+        outside_array.each_index do |i|
+          return i if i == 0
+        end
+      end
     end
 
     def withdraw(amount)

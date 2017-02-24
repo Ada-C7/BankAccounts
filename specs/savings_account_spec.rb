607e1701cd17d9060085fc44f1690e653e9b0bc3
jjousun <jjousun@gmail.com>
2017-02-23 17:50:59 -0800

    Wrote test for returning unchanged balance if would be below 0

--- specs/savings_account_spec.rb
+++ specs/savings_account_spec.rb
@@ -37,25 +37,43 @@ describe "SavingsAccount" do
     FEE = 2.00
 
     before do
-      @withdraw_amount = 25.0
+
       @balance = 100.0
       @account = Bank::SavingsAccount.new(12345, @balance)
-      @withdraw = @account.withdraw(@withdraw_amount)
+      #@withdraw = @account.withdraw(@withdraw_amount)
     end
 
     it "Applies a $2 fee each time" do
       # TODO: Your test code here!
-
-      expected_balance = @balance - @withdraw_amount - FEE
+      withdraw_amount = 25.0
+      expected_balance = @balance - withdraw_amount - FEE
+      @account.withdraw(withdraw_amount)
       @account.balance.must_equal expected_balance
     end
 
+    # Does not allow the account to go below the $10 minimum balance - Will output a warning message and return the original un-modified balance
     it "Outputs a warning if the balance would go below $10" do
       # TODO: Your test code here!
+      withdraw_amount = 95.0
+      # Another proc! This test expects something to be printed
+      # to the terminal, using 'must_output'. /.+/ is a regular
+      # expression matching one or more characters - as long as
+      # anything at all is printed out the test will pass.
+      proc {
+        @account.withdraw(withdraw_amount)
+      }.must_output /.+/
     end
 
     it "Doesn't modify the balance if it would go below $10" do
       # TODO: Your test code here!
+      withdraw_amount = 95.0
+
+      updated_balance = @account.withdraw(withdraw_amount)
+
+      # Both the value returned and the balance in the account
+      # must be un-modified.
+      updated_balance.must_equal @balance
+      @account.balance.must_equal @balance
     end
 
     it "Doesn't modify the balance if the fee would put it below $10" do

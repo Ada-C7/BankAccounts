194c3edebf696b95b02869300f709839a2b80fb8
sai samant <saisamant@gmail.com>
2017-02-24 13:37:51 -0800

    updated initialize method (streamlined if statement)

--- lib/savings_account.rb
+++ lib/savings_account.rb
@@ -1,28 +1,31 @@
 require_relative 'account.rb'
+
+
 module Bank
-class SavingsAccount < Bank::Account
-  def initialize id, balance, opendate = "1999-03-27 11:30:09 -0800"
-    # check_initial_balance
-    if balance >= 10
-      @balance = balance
-    else
-      raise ArgumentError.new "Initial balance must be at least $10."
+  class SavingsAccount < Account
+    def initialize id, balance, opendate = "1999-03-27 11:30:09 -0800"
+      # check_initial_balance
+      if balance <= 10
+        raise ArgumentError.new "Initial balance must be at least $10."
+      end
+      #initial balance meets threshold, follow account rules to initialize
+      super
     end
-    #initial balance meets threshold, follow account rules to initialize
-    super
+
+
+    def withdraw amount  #hoW can I use super?
+      # if @balance - amount - 2 <= 10
+
+      #new balance can't go below 10
+      #adds a transaction fee
+    end
+
+    def add_interest rate
+      interest = @balance * rate / 100
+      @balance += interest
+      return interest
+    end
+
+
   end
-
-
-  def withdraw amount
-    #new balance can't go below 10
-    #adds a transaction fee
-  end
-
-  def add_interest rate
-    # balance*rate/100
-    # return interest that was added (do not return updated balance)
-  end
-
-
-end
 end

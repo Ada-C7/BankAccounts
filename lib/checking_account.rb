832d53394266f2d96b380b3632e4ead3ea304ec3
jjousun <jjousun@gmail.com>
2017-02-24 14:02:47 -0800

    Fixed withdraw method to make sure FEE doesn't make it negative

--- lib/checking_account.rb
+++ lib/checking_account.rb
@@ -1,7 +1,15 @@
 module Bank
+  CH_FEE = 1.00
+
   class CheckingAccount < Bank::Account
-    # def initialize
-    #
-    # end
-  end
-end
+    def withdraw(withdraw_amount)
+      if @balance - withdraw_amount - CH_FEE < 0
+        print "Balance cannot be less than $0"
+        return @balance
+      else
+        @balance -= withdraw_amount + CH_FEE
+      end
+    end#end of withdraw def
+
+  end#end of Class
+end#end of Module

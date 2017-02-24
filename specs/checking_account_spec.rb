722e7a36252d01f65c05d4e8a13ef4d05078936e
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-02-24 14:14:06 -0800

    Completed all checking account tests

--- specs/checking_account_spec.rb
+++ specs/checking_account_spec.rb
@@ -1,80 +1,100 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
+require_relative '../lib/checking_account'
 
-# TODO: uncomment the next line once you start wave 3 and add lib/checking_account.rb
-# require_relative '../lib/checking_account'
+describe "CheckingAccount" do
 
-# Because a CheckingAccount is a kind
-# of Account, and we've already tested a bunch of functionality
-# on Account, we effectively get all that testing for free!
-# Here we'll only test things that are different.
-
-# TODO: change 'xdescribe' to 'describe' to run these tests
-xdescribe "CheckingAccount" do
-  xdescribe "#initialize" do
-    # Check that a CheckingAccount is in fact a kind of account
-    it "Is a kind of Account" do
-      account = Bank::CheckingAccount.new(12345, 100.0)
-      account.must_be_kind_of Bank::Account
-    end
+  before do
+    @account = Bank::CheckingAccount.new(12345, 100.0, nil)
   end
 
-  xdescribe "#withdraw" do
-    it "Applies a $1 fee each time" do
-      # TODO: Your test code here!
+    describe "#initialize" do
+
+      it "Is a kind of Account" do
+        @account.must_be_kind_of Bank::Account
+      end
     end
 
-    it "Doesn't modify the balance if the fee would put it negative" do
-      # TODO: Your test code here!
-    end
-  end
+    describe "#withdraw" do
 
-  xdescribe "#withdraw_using_check" do
+      it "Applies a $1 fee each time" do
+        @account.withdraw(20)
+        @account.balance.must_equal 79
+      end
+
+      it "Doesn't modify the balance if the fee would put it negative" do
+        proc {@account.withdraw(105.0)}.must_raise ArgumentError
+        @account.balance.must_equal 100
+      end
+    end
+
+  describe "#withdraw_using_check" do
     it "Reduces the balance" do
-      # TODO: Your test code here!
+      @account.withdraw_using_check(20)
+      @account.balance.must_equal 80
     end
 
     it "Returns the modified balance" do
-      # TODO: Your test code here!
+      @account.withdraw_using_check(20)
+      @account.balance.must_equal 80
     end
 
     it "Allows the balance to go down to -$10" do
-      # TODO: Your test code here!
+      @account.withdraw_using_check(110)
+      @account.balance.must_equal(-10)
     end
 
     it "Outputs a warning if the account would go below -$10" do
-      # TODO: Your test code here!
+      proc {@account.withdraw_using_check(120)}.must_raise ArgumentError
     end
 
     it "Doesn't modify the balance if the account would go below -$10" do
-      # TODO: Your test code here!
+      proc{@account.withdraw_using_check(120)}.must_raise ArgumentError
+      @account.balance.must_equal(100)
     end
 
     it "Requires a positive withdrawal amount" do
-      # TODO: Your test code here!
+      proc {@account.withdraw_using_check(-10)}.must_raise ArgumentError
     end
 
     it "Allows 3 free uses" do
-      # TODO: Your test code here!
+      3.times do
+        @account.withdraw_using_check(10)
+      end
+
+      @account.balance.must_equal(70)
     end
 
     it "Applies a $2 fee after the third use" do
-      # TODO: Your test code here!
+      4.times do
+        @account.withdraw_using_check(10)
+      end
+
+      @account.balance.must_equal(58)
     end
   end
 
-  xdescribe "#reset_checks" do
+  describe "#reset_checks" do
     it "Can be called without error" do
-      # TODO: Your test code here!
+      @account.reset_checks
+      @account.used_checks.must_equal 0
     end
 
     it "Makes the next three checks free if less than 3 checks had been used" do
-      # TODO: Your test code here!
+      3.times do
+        @account.withdraw_using_check(10)
+      end
+
+      @account.balance.must_equal(70)
     end
 
     it "Makes the next three checks free if more than 3 checks had been used" do
-      # TODO: Your test code here!
+      3.times do
+        @account.withdraw_using_check(10)
+      end
+
+      @account.balance.must_equal(70)
     end
   end
 end

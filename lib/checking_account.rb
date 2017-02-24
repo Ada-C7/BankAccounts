9ea4f9d708a8911d11181fc66efd536dbf08e02f
madamecurie <usermonalisa@gmail.com>
2017-02-24 00:36:32 -0800

    Describe methode withdraw in checking a/c.

--- lib/checking_account.rb
+++ lib/checking_account.rb
@@ -1,4 +1,21 @@
+require_relative 'account'
 module Bank
-  class CheckingAccount < Account
+  class CheckingAccount < Bank::Account
+    attr_reader :id, :balance
+
+    def initialize(id, balance)
+      @id = id
+      @balance = balance
+    end
+
+    def withdraw(amount)
+      new_balance = @balance - (amount+1)
+      if new_balance < 0
+        puts "Warning: insufficient fund."
+        return @balance
+      end
+      @balance = new_balance
+    end
+
   end
 end

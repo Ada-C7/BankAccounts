d7b50610be92dd2577e55d59d10a2bd19fc4c008
Lynn Trickey <lynn.trickey@gmail.com>
2017-02-24 12:20:13 -0800

    Adding files and stumps of Money Market account & spec files

--- lib/account.rb
+++ lib/account.rb
@@ -108,11 +108,13 @@ module Bank
           @owner = Bank::Owner.new(owner[0].to_i, owner[1], owner[2], owner[3], owner[4], owner[5])
         end
       end
+
       if @owner == nil
         raise ArgumentError.new "This owner does not exist!"
       else
         return @owner
       end
+
     end
 
   end
---
+++ lib/money_market_account.rb
@@ -0,0 +1,7 @@
+module Bank
+
+  class MoneyMarketAccount < Account
+    
+  end
+
+end

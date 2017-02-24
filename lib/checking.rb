b34ced5ce81a19df97ff7e2e7a56602d5e097e5f
ashtn <ahstonehorton@gmail.com>
2017-02-24 00:01:19 -0800

    Withdraw applies 1 dollar fee in checking

--- lib/checking.rb
+++ lib/checking.rb
@@ -7,10 +7,21 @@ require_relative 'account.rb'
 module Bank
   class CheckingAccount < Account
 
-    attr_accessor
+    #attr_accessor :balance
 
-    def initialize(account_hash)
+    def initialize(account)
       super
+    end
+
+    def withdraw(amount)
+
+      if @balance - (amount + 1) < 0
+        raise ArgumentError.new "Insufficient Funds"
+      end
+
+      @balance -= (amount + 1)
+      return @balance
+
     end
 
   end

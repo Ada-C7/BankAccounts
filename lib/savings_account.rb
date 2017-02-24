03184958e9e5e459c5151a4b38913141d4d09b10
Ann Dai <gofarann@gmail.com>
2017-02-23 16:42:18 -0800

    added add_interest functionality on SavingsAccount class, corrected initiation problems on test spec

--- lib/savings_account.rb
+++ lib/savings_account.rb
@@ -2,7 +2,7 @@ require 'csv'
 require_relative '../lib/account'
 module Bank
   class SavingsAccount < Bank::Account
-    attr_reader :balance, :id, :open_date
+    attr_reader :balance, :id, :open_date, :interest
 
 
     def initialize(id, balance, open_date)
@@ -19,20 +19,22 @@ module Bank
 
     def withdraw(withdrawl_amount)
       raise ArgumentError.new("withdrawl must be greater than 0") if withdrawl_amount < 0
-      fee = 2
-
-      if @balance - withdrawl_amount - fee >= 10
-        @balance -= (withdrawl_amount + fee)
+      if @balance - (withdrawl_amount + 2) >= 10
+        @balance = @balance - (withdrawl_amount + 2)
       else
         print "your balance will be less than $10"
-        return @balance += (@balance * rate/100)
+        return @balance
+
       end
     end
 
-
     def add_interest(rate)
-      rate = 0.25
-      @balance += @balance
+      if rate > 0
+        @interest = @balance * (rate/100)
+        @balance = @balance + @interest
+        return @interest.round(2)
+      else raise ArgumentError.new ("rate must be positive")
+      end
     end
   end
 end
--- specs/savings_account_spec.rb
+++ specs/savings_account_spec.rb
@@ -12,48 +12,110 @@ require_relative '../lib/savings_account'
 
 # TODO: change 'xdescribe' to 'describe' to run these tests
 describe "SavingsAccount" do
+
   describe "#initialize" do
+
     it "Is a kind of Account" do
+
+      id = 12345
+      starting_balance = 10
+      open_date = 20170101
+      account = Bank::SavingsAccount.new(id, starting_balance,open_date)
+
       # Check that a SavingsAccount is in fact a kind of account
       # edited to include 3 arguments
-      account = Bank::SavingsAccount.new(12345,100.0, 20170101)
       account.must_be_kind_of Bank::Account
     end
 
     it "Requires an initial balance of at least $10" do
       # TODO: Your test code here!
+      id = 12345
+      starting_balance = 9
+      open_date = 20170101
+
+
+      proc {
+        Bank::SavingsAccount.new(id, starting_balance, open_date)
+      }.must_raise ArgumentError
     end
   end
 
-  xdescribe "#withdraw" do
+  describe "#withdraw" do
+
+    before do
+
+      id = 12345
+      starting_balance = 100
+      open_date = 20170101
+      @account = Bank::SavingsAccount.new(id, starting_balance,open_date)
+
+    end
+
     it "Applies a $2 fee each time" do
       # TODO: Your test code here!
+      withdrawal_amount = 10
+      @account.withdraw(withdrawal_amount).must_equal 88
     end
 
     it "Outputs a warning if the balance would go below $10" do
       # TODO: Your test code here!
+      withdrawal_amount = 99
+      proc {
+        @account.withdraw(withdrawal_amount)
+      }.must_output /.+/
+
     end
 
     it "Doesn't modify the balance if it would go below $10" do
       # TODO: Your test code here!
+      @account.withdraw(99).must_equal 100
     end
+
 
     it "Doesn't modify the balance if the fee would put it below $10" do
       # TODO: Your test code here!
+      @account.withdraw(90).must_equal 100
+
     end
   end
 
-  xdescribe "#add_interest" do
+  describe "#add_interest" do
+
     it "Returns the interest calculated" do
+      id = 12345
+      starting_balance = 100
+      open_date = 20170101
+      rate = 0.25
+
+      account = Bank::SavingsAccount.new(id, starting_balance, open_date)
       # TODO: Your test code here!
+      account.add_interest(rate).must_equal 0.25
     end
 
     it "Updates the balance with calculated interest" do
+      id = 12345
+      starting_balance = 100
+      open_date = 20170101
+      rate = 0.25
+
+      account = Bank::SavingsAccount.new(id, starting_balance, open_date)
       # TODO: Your test code here!
+      account.add_interest(0.25)
+      account.balance.must_equal 100.25
     end
 
     it "Requires a positive rate" do
+      id = 12345
+      starting_balance = 100
+      open_date = 20170101
+      rate = 0
+      account = Bank::SavingsAccount.new(id, starting_balance, open_date)
+
       # TODO: Your test code here!
+      proc {
+        account.add_interest(rate)
+      }.must_raise ArgumentError
+
     end
   end
 end

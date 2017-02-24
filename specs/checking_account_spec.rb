0e8124091d3b0d899f7cd5b223e2425fa5b29d66
ashtn <ahstonehorton@gmail.com>
2017-02-24 01:58:05 -0800

    check_withdraw method passed all tests

--- specs/checking_account_spec.rb
+++ specs/checking_account_spec.rb
@@ -63,7 +63,7 @@ describe "CheckingAccount" do
       @account.check_withdraw(-100).must_equal 100
     end
 
-    it "Allows 3 free uses" do 
+    it "Allows 3 free uses" do
       @account.check_withdraw(20).must_equal 80
       @account.check_withdraw(20).must_equal 60
       @account.check_withdraw(20).must_equal 40
@@ -71,8 +71,12 @@ describe "CheckingAccount" do
 
     end
 
-    it "Applies a $2 fee after the third use" do skip
-      # TODO: Your test code here!
+    it "Applies a $2 fee after the third use" do
+      @account.check_withdraw(20).must_equal 80
+      @account.check_withdraw(20).must_equal 60
+      @account.check_withdraw(20).must_equal 40
+      @account.check_withdraw(20).must_equal 18
+      @account.check_withdraw(20).must_equal (-4)
     end
   end
 

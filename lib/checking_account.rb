22a36d3e4e476ffb8bd113a1146f00a16254c58b
sai samant <saisamant@gmail.com>
2017-02-23 20:45:57 -0800

    passes first CheckingAccount test

--- lib/checking_account.rb
+++ lib/checking_account.rb
@@ -1,21 +1,23 @@
-class CheckingAccount < Account
-  def initialize
-    super
-  end
-  
-  def withdraw amount  #this is updating the withdraw method in the Account class
-    # withdrawal fee of $1
-    # Does not allow the account to go negative. Will output a warning message and return the original un-modified balance.
-    #if > 3 checks, then charge a $2 check transaction fee
-    #update number of checks cashed counter
-  end
+module Bank
+  class CheckingAccount < Account
+    def initialize id, balance, opendate = "1999-03-27 11:30:09 -0800"
+      super
+    end
 
-  def reset_checks #new method
-    #resets number of checks cashed used to zero
+    def withdraw amount  #this is updating the withdraw method in the Account class
+      # withdrawal fee of $1
+      # Does not allow the account to go negative. Will output a warning message and return the original un-modified balance.
+      #if > 3 checks, then charge a $2 check transaction fee
+      #update number of checks cashed counter
+    end
+
+    def reset_checks #new method
+      #resets number of checks cashed used to zero
+    end
+
+    def withdraw_using_check amount  #new method
+    end
+
+
   end
-
-  def withdraw_using_check amount  #new method
-  end
-
-
 end
--- specs/checking_account_spec.rb
+++ specs/checking_account_spec.rb
@@ -1,6 +1,10 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
+require_relative '/Users/sai/Documents/ada/projects/BankAccounts/lib/account.rb'
+require_relative '/Users/sai/Documents/ada/projects/BankAccounts/lib/checking_account.rb'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # TODO: uncomment the next line once you start wave 3 and add lib/checking_account.rb
 # require_relative '../lib/checking_account'
@@ -11,7 +15,7 @@ require 'minitest/skip_dsl'
 # Here we'll only test things that are different.
 
 # TODO: change 'xdescribe' to 'describe' to run these tests
-xdescribe "CheckingAccount" do
+describe "CheckingAccount" do
   describe "#initialize" do
     # Check that a CheckingAccount is in fact a kind of account
     it "Is a kind of Account" do
@@ -20,7 +24,7 @@ xdescribe "CheckingAccount" do
     end
   end
 
-  describe "#withdraw" do
+  xdescribe "#withdraw" do
     it "Applies a $1 fee each time" do
       # TODO: Your test code here!
     end
@@ -30,7 +34,7 @@ xdescribe "CheckingAccount" do
     end
   end
 
-  describe "#withdraw_using_check" do
+  xdescribe "#withdraw_using_check" do
     it "Reduces the balance" do
       # TODO: Your test code here!
     end
@@ -64,7 +68,7 @@ xdescribe "CheckingAccount" do
     end
   end
 
-  describe "#reset_checks" do
+  xdescribe "#reset_checks" do
     it "Can be called without error" do
       # TODO: Your test code here!
     end

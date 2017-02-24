ddd693f58ebdd5aa417134d1bfd26c7fc9e20a8c
add2point71dots <alcland@gmail.com>
2017-02-24 15:37:02 -0800

    Adds and tests withdraw method

--- lib/money_market_account.rb
+++ lib/money_market_account.rb
@@ -1,7 +1,31 @@
 require_relative 'account'
 
 module Bank
-  class MoneyMarketAccount
+  class MoneyMarketAccount  < Bank::Account
+    def initialize(id, balance, date)
 
+      #interpreting 1000000 as $10,000.00 because of how the csv file defined balance
+      raise ArgumentError.new "Balance must be at least 1000000" if balance < 1000000
+
+      super(id, balance, date)
+      @num_transactions = 0
+    end
+
+    def withdraw(amount)
+      if @balance < 1000000
+        puts "Your cannot make withdrawals when your balance is below 1000000."
+        return @balance
+
+      elsif (@balance - amount - 10000) < 0
+        puts "You can't withdraw that much"
+        return @balance
+      end
+
+      super(amount)
+      @balance -= 10000 if (@balance < 1000000)
+      
+      @num_transactions += 1
+      return @balance
+    end
   end
 end
--- specs/money_market_account_spec.rb
+++ specs/money_market_account_spec.rb
@@ -1,12 +1,47 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
-require_relative '../lib/money_market'
+require_relative '../lib/money_market_account'
 
-describe "SavingsAccount" do
+describe "MoneyMarketAccount" do
   before do
-    @account = Bank::MoneyMarketAccount.new(12345, 10000, "1999-03-27 11:30:09 -0800")
+    @account = Bank::MoneyMarketAccount.new(12345, 2000000, "1999-03-27 11:30:09 -0800")
   end
 
+  describe "#initialize" do
+    it "Is a kind of Account" do
 
+      # Check that a MoneyMarketAccount is in fact a kind of account
+      @account.must_be_kind_of Bank::Account
+    end
+
+    it "Requires an initial balance of at least $10,000" do
+      proc {
+        Bank::MoneyMarketAccount.new(12345, 999999, "1999-03-27 11:30:09 -0800")
+      }.must_raise ArgumentError
+    end
+  end
+
+  describe "#withdraw" do
+    it "Outputs a warning if the account would go negative with the fee" do
+      proc { @account.withdraw(1990001) }.must_output (/.+/)
+    end
+
+    it "Doesn't modify the balance if the $100 fee makes the account go negative" do
+      updated_balance = @account.withdraw(1990001)
+      updated_balance.must_equal 2000000
+      @account.balance.must_equal 2000000
+    end
+
+    it "Allows the balance to go to zero" do
+      updated_balance = @account.withdraw(@account.balance - 10000)
+      updated_balance.must_equal 0
+      @account.balance.must_equal 0
+    end
+
+    it "Doesn't allow withdraws if account goes below 1000000" do
+      @account.withdraw(1000001)
+      proc { @account.withdraw(1) }.must_output (/.+/)
+    end
+  end
 end

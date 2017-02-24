6552935da16fd17837cb14606fad85af67553925
sai samant <saisamant@gmail.com>
2017-02-23 17:05:06 -0800

    added methods with pseudocode based on reqs in Wave 3

--- lib/savings_account.rb
+++ lib/savings_account.rb
@@ -1,2 +1,14 @@
 class SavingsAccount
+
+  def withdraw amount
+    #new balance can't go below 10
+    #adds a transaction fee
+  end
+
+  def add_interest rate
+    # balance*rate/100
+    # return interest that was added (do not return updated balance)
+  end
+
+
 end

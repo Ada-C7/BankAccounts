b63ad1c16cc6cbce0ae3fc14d997602cd63e6828
natalia-ku <bilberryfm@mail.ru>
2017-02-23 20:13:04 -0800

    Added method withdraw_with_check in CheckAccount Class and tests for test this method

--- specs/checking_account_spec.rb
+++ specs/checking_account_spec.rb
@@ -1,17 +1,10 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
+require_relative '../lib/checking_account'
 
-# TODO: uncomment the next line once you start wave 3 and add lib/checking_account.rb
-# require_relative '../lib/checking_account'
 
-# Because a CheckingAccount is a kind
-# of Account, and we've already tested a bunch of functionality
-# on Account, we effectively get all that testing for free!
-# Here we'll only test things that are differxnent.
-
-# TODO: change 'xdescribe' to 'describe' to run these tests
-xdescribe "CheckingAccount" do
+describe "CheckingAccount" do
   describe "#initialize" do
     # Check that a CheckingAccount is in fact a kind of account
     it "Is a kind of Account" do
@@ -22,45 +15,89 @@ xdescribe "CheckingAccount" do
 
   describe "#withdraw" do
     it "Applies a $1 fee each time" do
-      # TODO: Your test code here!
+      account = Bank::CheckingAccount.new(1234, 100)
+      amount_to_withdraw = 10
+      expected_result = account.balance - amount_to_withdraw - 1
+      account.withdraw(amount_to_withdraw).must_equal expected_result
     end
 
     it "Doesn't modify the balance if the fee would put it negative" do
-      # TODO: Your test code here!
+      balance = 100
+      amount_to_withdraw = 100
+      account = Bank::CheckingAccount.new(1234, balance)
+      puts account.withdraw(amount_to_withdraw)
+      account.withdraw(amount_to_withdraw).must_equal balance
     end
   end
 
   describe "#withdraw_using_check" do
     it "Reduces the balance" do
-      # TODO: Your test code here!
+      account = Bank::CheckingAccount.new(1234, 100)
+      balance = account.balance
+      result = account.withdraw_using_check(50)
+      result.must_be :<, balance
     end
 
     it "Returns the modified balance" do
-      # TODO: Your test code here!
+      account = Bank::CheckingAccount.new(1234, 100)
+      amount = 10
+      result = account.withdraw_using_check(amount)
+      expected_result =  100 - amount
+      result.must_equal expected_result
     end
 
     it "Allows the balance to go down to -$10" do
-      # TODO: Your test code here!
+      account = Bank::CheckingAccount.new(1234, 100)
+      amount_to_withdraw = 110
+      result = account.withdraw_using_check(amount_to_withdraw)
+      result.must_equal -10
     end
 
     it "Outputs a warning if the account would go below -$10" do
-      # TODO: Your test code here!
+      account = Bank::CheckingAccount.new(1234, 100)
+      amount_to_withdraw = 90
+      proc {
+        account.withdraw_using_check(amount_to_withdraw)
+      }.must_output /.+/
     end
 
     it "Doesn't modify the balance if the account would go below -$10" do
-      # TODO: Your test code here!
+      balance = 100
+      amount_to_withdraw = 111
+      account = Bank::CheckingAccount.new(1234, balance)
+      account.withdraw_using_check(amount_to_withdraw).must_equal 100
     end
 
     it "Requires a positive withdrawal amount" do
-      # TODO: Your test code here!
+      start_balance = 100
+      withdrawal_amount = -25
+      account = Bank::CheckingAccount.new(1337, start_balance)
+      proc {
+        account.withdraw_using_check(withdrawal_amount)
+      }.must_raise ArgumentError
     end
 
     it "Allows 3 free uses" do
-      # TODO: Your test code here!
+      amount = 10
+      account = Bank::CheckingAccount.new(1337, 100)
+      account.withdraw_using_check(amount)
+      account.withdraw_using_check(amount)
+      account.withdraw_using_check(amount)
+      account.balance.must_equal 70
+      account.withdraw_using_check(amount)
+      account.balance.must_equal 58
     end
 
     it "Applies a $2 fee after the third use" do
-      # TODO: Your test code here!
+      amount = 10
+      fee = 2
+      account = Bank::CheckingAccount.new(1337, 100)
+      account.withdraw_using_check(amount)
+      account.withdraw_using_check(amount)
+      account.withdraw_using_check(amount)
+      result = account.withdraw_using_check(amount)
+      expected_result = 100 - (4*10) - fee
+      result.must_equal expected_result
     end
   end
 

817981fcb33bf2916f7630898b5ef7a5965f9eae
EricaJCase <ericajcase@gmail.com>
2017-02-24 16:00:33 -0800

    Made checks_used an instance variable instead of a class variable

--- lib/checking_account.rb
+++ lib/checking_account.rb
@@ -2,7 +2,7 @@ module Bank
   class CheckingAccount < Account
     def initialize(id, balance, date)
       super(id, balance, date)
-      @@withdrawals_by_check = 0
+      @withdrawals_by_check = 0
     end
 
     def withdraw(amount, fee = 100, min_balance = 0)
@@ -13,20 +13,20 @@ module Bank
     def withdraw_using_check(amount)
       balance_before_withdrawal = @balance
 
-      if @@withdrawals_by_check > 2
+      if @withdrawals_by_check > 2
         self.withdraw(amount, fee = 200, min_balance = -1000 )
       else
         self.withdraw(amount, fee = 0, min_balance = -1000 )
       end
 
       if @balance < balance_before_withdrawal
-        @@withdrawals_by_check += 1
+        @withdrawals_by_check += 1
       end
       @balance =  balance
     end
 
     def reset_checks
-      @@withdrawals_by_check = 0
+      @withdrawals_by_check = 0
     end
   end
 end

33522c85cf601c4bb854d369412d760c79b6f526
jjousun <jjousun@gmail.com>
2017-02-25 11:45:30 -0800

    Fixed method for withdraw using checks, logic was off by 1 when going through iteration when count starts at 0

--- lib/checking_account.rb
+++ lib/checking_account.rb
@@ -1,7 +1,15 @@
 module Bank
   CH_FEE = 1.00
+  OVR_FEE = 2.00
 
   class CheckingAccount < Bank::Account
+    attr_reader :count
+
+    def initialize(id, balance)
+      super(id, balance)
+      @count = 0
+    end
+
     def withdraw(withdraw_amount)
       if @balance - withdraw_amount - CH_FEE < 0
         print "Balance cannot be less than $0"
@@ -10,6 +18,31 @@ module Bank
         @balance -= withdraw_amount + CH_FEE
       end
     end#end of withdraw def
+
+    def withdraw_using_check(amount)
+      if amount < 0
+        print "Withdrawal must be positive"
+      end
+      if @balance - amount < -10.0
+        print "Overdraft limit of -$10.00 is reached"
+        return @balance
+      elsif @count < 3
+        @count += 1
+        @balance -= amount
+      elsif @count >= 3
+        @count += 1
+        @balance -= amount + 2.00
+      end#end of if loop
+    end#end of withdraw_using_check method
+
+    # def withdraw_using_check_call_count
+    #   @count = 0
+    #   name = :withdraw_using_check
+    #   TracePoint.trace(:call) do |t|
+    #     @count += 1 if t.method_id == name
+    #   end
+    #    return @count
+    # end #end of count method
 
   end#end of Class
 end#end of Module

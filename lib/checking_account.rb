4f219a0a59fff9be381f5a38514ddf57ad430717
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-02-25 00:07:26 -0800

    Wave3/ withdraw_using_check mehtod passed the test

--- lib/checking_account.rb
+++ lib/checking_account.rb
@@ -1,7 +1,9 @@
 module Bank
   class CheckingAccount < Account
+    #attr_reader :check_count
     def initialize(id, initial_balance, open_date = 0, owner_id = -1)
       super
+      @checks_processed = 0
     end
 
     def withdraw(amount)
@@ -11,7 +13,26 @@ module Bank
       else
         puts "Insufficient funds, balance would go negative."
       end
+      return @balance
+    end
 
+    def withdraw_using_check(amount)
+      raise ArgumentError.new "Invalid negative amount" if amount < 0
+      max_overdraft = -10
+      # $2 check fee per check after 3 checks
+      # in ONE month
+      if @checks_processed <= 3
+        check_fee = 0.0
+      else
+        check_fee = 2.0
+      end
+
+      if (@balance - amount - check_fee) >= max_overdraft
+        @balance -= (amount + check_fee)
+        @checks_processed += 1
+      else
+        print "Check ($#{amount}) would make your account go below -$10."
+      end
       return @balance
     end
 
--- specs/checking_account_spec.rb
+++ specs/checking_account_spec.rb
@@ -13,7 +13,7 @@ require_relative "../lib/checking_account"
 # Here we'll only test things that are different.
 
 # TODO: change 'xdescribe' to 'describe' to run these tests
-describe "CheckingAccount" do
+#describe "CheckingAccount" do
   describe "#initialize" do
     # Check that a CheckingAccount is in fact a kind of account
     it "Is a kind of Account" do
@@ -39,43 +39,115 @@ describe "CheckingAccount" do
       new_balance = account.withdraw(withdrawal_amount)
       new_balance.must_equal initial_balance
     end
-  end  
+  end
 
-  xdescribe "#withdraw_using_check" do
+  describe "#withdraw_using_check" do
+    # before do
+    #   @account =
+    # end
     it "Reduces the balance" do
       # TODO: Your test code here!
+      start_balance = 100.0
+      withdrawal_amount = 25.0
+      #open_date = '1/1/2017'
+      account = Bank::CheckingAccount.new(1337, start_balance)
+
+      account.withdraw_using_check(withdrawal_amount)
+
+      expected_balance = start_balance - withdrawal_amount
+      account.balance.must_equal expected_balance
     end
 
     it "Returns the modified balance" do
-      # TODO: Your test code here!
+      start_balance = 100.0
+      withdrawal_amount = 25.0
+
+      account = Bank::CheckingAccount.new(1337, start_balance)
+
+      updated_balance = account.withdraw_using_check(withdrawal_amount)
+
+      expected_balance = start_balance - withdrawal_amount
+      updated_balance.must_equal expected_balance
     end
 
     it "Allows the balance to go down to -$10" do
-      # TODO: Your test code here!
+      start_balance = 100.0
+      withdrawal_amount = 110.0
+      max_overdraft = -10.0
+
+      account = Bank::CheckingAccount.new(1337, start_balance)
+
+      updated_balance = account.withdraw_using_check(withdrawal_amount)
+
+      updated_balance.must_equal max_overdraft
+
     end
 
     it "Outputs a warning if the account would go below -$10" do
-      # TODO: Your test code here!
+      start_balance = 100.0
+      withdrawal_amount = 120.0
+
+      account = Bank::CheckingAccount.new(1337, start_balance)
+
+      proc {account.withdraw_using_check(withdrawal_amount)}.must_output "Check ($#{withdrawal_amount}) would make your account go below -$10."
     end
 
     it "Doesn't modify the balance if the account would go below -$10" do
-      # TODO: Your test code here!
+      start_balance = 100.0
+      withdrawal_amount = 120.0
+
+      account = Bank::CheckingAccount.new(1337, start_balance)
+
+      account.withdraw_using_check(withdrawal_amount).must_equal start_balance
     end
 
     it "Requires a positive withdrawal amount" do
-      # TODO: Your test code here!
+      start_balance = 100.0
+      withdrawal_amount = -10.0
+
+      account = Bank::CheckingAccount.new(1337, start_balance)
+
+      proc {account.withdraw_using_check(withdrawal_amount)
+      }.must_raise ArgumentError
     end
 
     it "Allows 3 free uses" do
-      # TODO: Your test code here!
+      start_balance = 100.0
+      withdrawal_amount = 10.0
+
+      check_uses = 3
+      check_fee = 0.0
+      total_withdrawal = withdrawal_amount * check_uses
+
+      account = Bank::CheckingAccount.new(1337, start_balance)
+
+      check_uses.times do
+        account.withdraw_using_check(withdrawal_amount)
+      end
+
+      account.balance.must_equal (start_balance-total_withdrawal)
+
     end
 
     it "Applies a $2 fee after the third use" do
-      # TODO: Your test code here!
+      start_balance = 100.0
+      withdrawal_amount = 10.0
+
+      check_uses = 4
+      check_fee = 2.0
+      total_withdrawal = withdrawal_amount * check_uses
+
+      account = Bank::CheckingAccount.new(1337, start_balance)
+
+      check_uses.times do
+        account.withdraw_using_check(withdrawal_amount)
+      end
+
+      account.balance.must_equal (start_balance-total_withdrawal)
     end
   end
 
-  describe "#reset_checks" do
+  xdescribe "#reset_checks" do
     it "Can be called without error" do
       # TODO: Your test code here!
     end
@@ -88,4 +160,3 @@ describe "CheckingAccount" do
       # TODO: Your test code here!
     end
   end
-end

0c10fa2b77aeae5644573aed0148d29abc23a46f
Spatterjaaay <aja.pursova@gmail.com>
2017-02-26 12:32:51 -0800

    Updated withdraw method and added error messages

--- lib/checking_account.rb
+++ lib/checking_account.rb
@@ -1,5 +1,3 @@
-#require_relative 'account'
-
 module Bank
   class CheckingAccount < Account
     attr_reader :used_checks
@@ -8,29 +6,35 @@ module Bank
       super(id, start_balance)
 
       @used_checks = 0
-
     end
 
     def withdraw(withdrawal_amount)
       fee = 1.0
 
       if @balance - (withdrawal_amount + fee) < 0
-        print "You're withdrawing too much."
+        print "You're withdrawing too much"
         return @balance
       else
-        @balance -= withdrawal_amount + fee
+        # @balance -= withdrawal_amount + fee
+        # have to do super for withdrawal amount and fee separately
+        # in case the user inputed negative value that's lower than 1
+        # because adding one to it would amke it positive
+        super(withdrawal_amount)
+        super(fee)
       end
+
     end
 
     def withdraw_using_check(withdrawal_amount)
       fee = 2.0
 
       if withdrawal_amount < 0
-        raise ArgumentError
+        raise ArgumentError.new "Withdrawal amount must be positive"
       elsif @balance - withdrawal_amount < -10.0
-        print "You are withdrawing too much!"
+        print "You are withdrawing too much"
         return @balance
-      elsif @used_checks += 1
+      else
+       @used_checks += 1
        @balance -= fee if @used_checks > 3
        @balance -= withdrawal_amount
       end

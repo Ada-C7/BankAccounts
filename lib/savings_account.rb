f7540e44eece1ffe7778fe88d1f5d73b72a60bfb
Spatterjaaay <aja.pursova@gmail.com>
2017-02-26 12:34:08 -0800

    Updated withdraw method and added error messages

--- lib/savings_account.rb
+++ lib/savings_account.rb
@@ -1,38 +1,40 @@
 module Bank
   class SavingsAccount < Account
+
     def initialize(id, start_balance)
       super(id, start_balance)
 
       if start_balance >= 10.0
         @balance = start_balance
-      else raise ArgumentError.new
+      else
+        raise ArgumentError.new "New balance must be equal or greater than 10"
       end
+
     end
 
     def withdraw(withdrawal_amount)
-      # Does not allow the account to go below the $10 minimum balance
-      # Will output a warning message
-      # return the original un-modified balan.0ce
       fee = 2.0
+
       if @balance - (withdrawal_amount + fee) < 10
         print "Balance cannot be under $10"
         return @balance
       else
-        @balance -= withdrawal_amount + fee
+        # @balance -= withdrawal_amount + fee
+        super(withdrawal_amount)
+        super(fee)
       end
 
     end
 
     def add_interest(rate)
       if rate < 0
-        raise ArgumentError
+        raise ArgumentError.new "Interest rate must be positive"
       else
         interest = @balance * rate/100
         @balance += interest
         return interest
       end
     end
-
 
   end
 end

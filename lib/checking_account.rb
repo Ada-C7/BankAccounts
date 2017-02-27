5866a4fab8592aba98a0fd56f0b89720db562008
Ting Wong <hellotingwong@gmail.com>
2017-02-26 16:11:53 -0800

    Completed Mandatory

--- lib/checking_account.rb
+++ lib/checking_account.rb
@@ -2,6 +2,7 @@ module Bank
   class CheckingAccount < Account
     def initialize(id, balance, open_date = '2012 - 12-21 12:21:12 -0800')
       super(id, balance, open_date)
+      @check_count = 1
     end
 
     def withdraw(amount)
@@ -16,15 +17,19 @@ module Bank
 
     def withdraw_using_check(amount)
       new_balance = @balance - amount
-      check_count = 0
       if new_balance < -10
-        # puts "You don't have enough money in your account"
-      else
+        puts "You don't have enough money in your account"
+      elsif @check_count <= 3
         raise ArgumentError, "Withdrawals should be a positive number" unless amount >=0
-        check_count += 1
+        @check_count += 1
         return @balance = @balance - amount
+      elsif @check_count > 3
+        return @balance = @balance - amount - 2
       end
+    end
 
+    def reset_checks
+      return @check_count = 1
     end
 
   end

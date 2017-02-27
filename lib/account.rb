0f667ded537c3adce6751538b2ac9559db80bf82
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-02-26 23:40:27 -0800

    Chris's Wave 2 code

--- lib/account.rb
+++ lib/account.rb
@@ -1,13 +1,44 @@
+require 'csv'
+
 module Bank
   class Account
-    attr_reader :id, :balance
-    def initialize(id, balance)
-      #error message not appearing in def withdraw
-      #WOW, that balance can't be @balance. WHY?
-      raise ArgumentError.new("balance must be >= 0") if balance < 0
+    attr_reader :id, :balance, :open_date
 
+    def initialize(id, balance, open_date = nil)
+
+      raise ArgumentError.new("balance must be >= 0") if balance < 0
       @id = id
       @balance = balance
+      @open_date = open_date
+    end
+
+    def self.all #better name! .create_all_accounts_from_csv
+      #.all is JUST the class method name. Like the instance method names
+      accounts = []
+
+      CSV.read("support/accounts.csv").each do |line|
+        id = line[0].to_i
+        balance = line[1].to_i
+        open_date = line[2]
+
+        account_instance = Bank::Account.new(id, balance, open_date)
+
+        accounts << account_instance
+      end
+
+      accounts
+    end
+
+    def self.find(id)
+      accounts = Bank::Account.all
+
+      accounts.each do |account|
+        if account.id == id
+          return account
+        end
+      end
+
+      raise ArgumentError.new "Account: #{id} does not exist"
     end
 
     def withdraw(amount)
